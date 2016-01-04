c5c7cef0d22509b4185142cf838c756e12f2de07
Matt Gardner <mattg@allenai.org>
2016-01-04 10:58:35 -0800

    More lisp formatting
    Former-commit-id: 35507ecd85595246839c8e611c7991984b6ab080

--- src/lisp/environment.lisp
+++ src/lisp/environment.lisp
@@ -9,13 +9,13 @@
 ;; Combines the elements of seq using f.
 (define foldr (lambda (f seq init)
                 (if (nil? seq)
-                    init
+                  init
                   (f (car seq) (foldr f (cdr seq) init)))))
 
 (define lifted-foldr (lambda (f seq init)
-                (if (nil? seq)
-                    init
-                  (f (lifted-car seq) (lifted-foldr f (lifted-cdr seq) init)))))
+                       (if (nil? seq)
+                         init
+                         (f (lifted-car seq) (lifted-foldr f (lifted-cdr seq) init)))))
 
 (define zip (list1 list2) (if (nil? list1) (list) (cons (list (car list1) (car list2)) (zip (cdr list1) (cdr list2)))))
 (define lifted-zip (list1 list2) (if (nil? list1) (lifted-list) (lifted-cons (lifted-list (lifted-car list1) (lifted-car list2)) (lifted-zip (lifted-cdr list1) (lifted-cdr list2)))))
@@ -33,7 +33,7 @@
 (define length (lambda (seq) (if (nil? seq) 0 (+ (length (cdr seq)) 1))))
 (define first-n (lambda (seq n) (if (= n 0) (list) (cons (car seq) (first-n (cdr seq) (- n 1))))))
 (define remainder-n (lambda (seq n) (if (= n 0) seq (remainder-n (cdr seq) (- n 1)))))
-(define get-ith-element (lambda (seq i) (if (nil? seq) (list) 
+(define get-ith-element (lambda (seq i) (if (nil? seq) (list)
                                           (if (= i 0) (car seq)
                                             (get-ith-element (cdr seq) (- i 1))))))
 
@@ -42,51 +42,51 @@
 (define find-index (elt seq) (find-index-helper elt seq 0))
 (define find-index-helper (elt seq i)
   (if (nil? seq)
-      -1
+    -1
     (if (= (car seq) elt)
-        i
+      i
       (find-index-helper elt (cdr seq) (+ i 1)))))
 
-(define lifted-get-ith-element (lambda (seq i) (if (nil? seq) (lifted-list) 
+(define lifted-get-ith-element (lambda (seq i) (if (nil? seq) (lifted-list)
                                                  (if (= i 0) (lifted-car seq)
                                                    (lifted-get-ith-element (lifted-cdr seq) (- i 1))))))
 
 (define lifted-cadr (x) (lifted-car (lifted-cdr x)))
 
-(define lifted-alist-find (elt alist) 
+(define lifted-alist-find (elt alist)
   (if (nil? alist)
-      (lifted-list)
+    (lifted-list)
     (if (= (lifted-car (lifted-car alist)) elt)
-        (lifted-car (lifted-cdr (lifted-car alist)))
+      (lifted-car (lifted-cdr (lifted-car alist)))
       (lifted-alist-find elt (lifted-cdr alist)))))
 
 (define lifted-list-eq? (l1 l2)
   (if (or (nil? l1) (nil? l2))
-      (and (nil? l1) (nil? l2))
+    (and (nil? l1) (nil? l2))
     (and (= (lifted-car l1) (lifted-car l2))
          (lifted-list-eq? (cdr l1) (cdr l2)))))
 
 ;; Appends two lists.
 (define append (lambda (l1 l2)
                  (if (nil? l1)
-                     l2
-                     (cons (car l1) (append (cdr l1) l2)))))
+                   l2
+                   (cons (car l1) (append (cdr l1) l2)))))
 
 (define flatten (lambda (list-of-lists)
                   (if (nil? list-of-lists)
-                      (list)
+                    (list)
                     (append (car list-of-lists) (flatten (cdr list-of-lists))))))
 
 ;; Takes a list of lists and returns a list of tuples of every possible
 ;; pair (triplet, etc.) of elements.
-(define outer-product (list-of-lists) 
+(define outer-product (list-of-lists)
   (if (nil? list-of-lists)
-      (list (list))
+    (list (list))
     (outer-product-helper (car list-of-lists) (outer-product (cdr list-of-lists)))))
 
 (define outer-product-helper (input rest)
   (if (nil? input)
-      (list)
+    (list)
     (append (map (lambda (x) (cons (car input) x)) rest)
             (outer-product-helper (cdr input) rest))))
 
@@ -95,7 +95,7 @@
 
 (define generate-int-seq (start end)
   (if (or (> start end) (= start end))
-      (list)
+    (list)
     (cons start (generate-int-seq (+ start 1) end))))
 
 ;; Forces all nondeterministic executions to satisfy the given condition
