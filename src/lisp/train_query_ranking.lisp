dc29a8fcdc6afa7a5ca0ae1e9ee1d2c740f3a848
Matt Gardner <mattg@allenai.org>
2016-01-04 10:34:11 -0800

    Just formatting stuff here, pretty much.
    Former-commit-id: e2765d5cb17abdfd5c811b77fa51f2210cbb3b55

--- src/lisp/train_query_ranking.lisp
+++ src/lisp/train_query_ranking.lisp
@@ -6,17 +6,17 @@
 (display "Making training data...")
 
 (define training-data (array-zip training-inputs
-                           (array-map (lambda (x) #t) training-inputs)))
+                                 (array-map (lambda (x) #t) training-inputs)))
 
 ; (define random-cat-neg-example (cat-word-pred)
 ;   (not (cat-word-pred (dictionary-rand-elt entities))))
-; 
+;
 ; (define weighted-random-cat-neg-example (cat-word-pred)
 ;   (not (cat-word-pred (sample-histogram entity-histogram))))
-; 
+;
 ; (define random-rel-neg-example (rel-word-pred)
 ;   (not (apply rel-word-pred (dictionary-rand-elt entity-tuples))))
-; 
+;
 ; (define weighted-random-rel-neg-example (rel-word-pred)
 ;   (not (apply rel-word-pred (sample-histogram entity-tuple-histogram))))
 
@@ -25,26 +25,26 @@
   (lambda (word)
     (lambda (entity neg-entity)
       (if (dictionary-contains entity entities)
-	  (let ((var (make-entity-var entity)))
-	    (make-ranking-inner-product-classifier 
-	     var #t (get-cat-word-params word word-parameters) (get-entity-params entity entity-parameters)
-	     (get-entity-params neg-entity entity-parameters))
-	    var)
-	  #f)
+        (let ((var (make-entity-var entity)))
+          (make-ranking-inner-product-classifier
+            var #t (get-cat-word-params word word-parameters) (get-entity-params entity entity-parameters)
+            (get-entity-params neg-entity entity-parameters))
+          var)
+        #f)
       )))
 
 (define word-rel-ranking-family (word-rel-params entity-tuple-params)
   (define word-rel (word)
     (lambda (entity1 neg-entity1 entity2 neg-entity2)
       (if (dictionary-contains (list entity1 entity2) entity-tuples)
-	  (let ((var (make-entity-var (cons entity1 entity2))))
-	    (make-ranking-inner-product-classifier 
-	     var #t (get-rel-word-params word word-rel-params)
-	     (get-entity-tuple-params entity1 entity2 entity-tuple-params)
-	     (get-entity-tuple-params neg-entity1 neg-entity2 entity-tuple-params))
-	    var)
-	  #f
-	  )
+        (let ((var (make-entity-var (cons entity1 entity2))))
+          (make-ranking-inner-product-classifier
+            var #t (get-rel-word-params word word-rel-params)
+            (get-entity-tuple-params entity1 entity2 entity-tuple-params)
+            (get-entity-tuple-params neg-entity1 neg-entity2 entity-tuple-params))
+          var)
+        #f
+        )
       ))
   word-rel)
 
@@ -57,36 +57,36 @@
         (word-rel (word-rel-ranking-family word-rel-parameters entity-tuple-parameters)))
     (define expression-evaluator (expression arg-entities arg1s arg2s positive-set-ind)
       (let ((arg1-candidates (apply dset-intersect (map (lambda (x) (array-get-ith-element arg1-arg2-map (dictionary-lookup x entities))) arg1s)))
-	    (arg2-candidates (apply dset-intersect (map (lambda (x) (array-get-ith-element arg2-arg1-map (dictionary-lookup x entities))) arg2s)))
-	    (all-candidates (dset-intersect arg1-candidates arg2-candidates))
-	    (positive-examples (array-get-ith-element joint-entities positive-set-ind)))
+            (arg2-candidates (apply dset-intersect (map (lambda (x) (array-get-ith-element arg2-arg1-map (dictionary-lookup x entities))) arg2s)))
+            (all-candidates (dset-intersect arg1-candidates arg2-candidates))
+            (positive-examples (array-get-ith-element joint-entities positive-set-ind)))
 
-	(if (nil? all-candidates)
-;; No constraints on the argument value.
-	    (begin
-	      ((eval expression) (car arg-entities) (rejection-sample-histogram entity-histogram positive-examples))
-	      )
+        (if (nil? all-candidates)
+          ;; No constraints on the argument value.
+          (begin
+            ((eval expression) (car arg-entities) (rejection-sample-histogram entity-histogram positive-examples))
+            )
 
-	    (let ((new-candidates (dset-subtract all-candidates positive-examples)))
-	      (if (not (dset-empty? new-candidates))
-		  ((eval expression) (car arg-entities) (sample-histogram-conditional entity-histogram new-candidates))
-; This #f results in a search error. The search error happens because
-; every possible value for the sampled entity has occurred as an answer to this query.
-		  #f
-		  )
-	      )
-	    )
-	))
+          (let ((new-candidates (dset-subtract all-candidates positive-examples)))
+            (if (not (dset-empty? new-candidates))
+              ((eval expression) (car arg-entities) (sample-histogram-conditional entity-histogram new-candidates))
+              ; This #f results in a search error. The search error happens because
+              ; every possible value for the sampled entity has occurred as an answer to this query.
+              #f
+              )
+            )
+          )
+        ))
     expression-evaluator))
 
 
 (display "Made training data.")
-(define expression-parameters (make-parameter-list (list 
-                                                    (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array cat-words)))
-                                                    (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array entities)))
-                                                    (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array rel-words)))
-                                                    (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array entity-tuples)))
-                                    )))
+(define expression-parameters (make-parameter-list (list
+                                                     (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array cat-words)))
+                                                     (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array entities)))
+                                                     (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array rel-words)))
+                                                     (make-parameter-list (array-map (lambda (x) (make-vector-parameters latent-dimensionality)) (dictionary-to-array entity-tuples)))
+                                                     )))
 (perturb-parameters expression-parameters 0.1)
 
 (display "Training...")
--- src/lisp/universal_schema.lisp
+++ src/lisp/universal_schema.lisp
@@ -1,12 +1,12 @@
-;; Requires the user to define the following variables 
+;; Requires the user to define the following variables
 ;; in a data file before running this program:
 ;; entities -- e.g. (define entities (list "/en/plano" "/en/Texas" "/en/banana"))
 ;; cat-words    -- e.g. (define words (list "city" "place" "fruit" "in"))
-;; training-inputs -- e.g., 
+;; training-inputs -- e.g.,
 ;;   (define training-inputs
 ;;     (list (list (quote (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x))) cur-entities)) entities)
-;;           (list (quote (exists-func (lambda (y) (and ((mention "/en/Texas") y) 
-;;                                                (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x)) ((word-rel "in") x y)) cur-entities))) 
+;;           (list (quote (exists-func (lambda (y) (and ((mention "/en/Texas") y)
+;;                                                (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x)) ((word-rel "in") x y)) cur-entities)))
 ;;                               cur-entities)) entities)
 ;;           ))
 
@@ -30,7 +30,7 @@
   (array-map (lambda (entity) (predicate entity)) candidate-set))
 
 ;; Returns true if any element of set1 is true.
-(define any-is-true? (set1) 
+(define any-is-true? (set1)
   (array-foldr or set1 #f))
 
 ;; Returns true if func returns true for any candidate
@@ -55,59 +55,59 @@
 ;; Returns true if all elements of set1 are present in set2.
 ;; Equivalent to set1 \subset set2
 (define contains-all? (set1 set2)
-  (lifted-foldr 
-   and
-   (lifted-map (lambda (tuple) (=> (lifted-car tuple) (lifted-cadr tuple)))
-               (lifted-zip set1 set2))
-   #t))
+  (lifted-foldr
+    and
+    (lifted-map (lambda (tuple) (=> (lifted-car tuple) (lifted-cadr tuple)))
+                (lifted-zip set1 set2))
+    #t))
 
 (define get-elementwise-marginals (predicate-amb-list)
   (lifted-map (lambda (x) (get-marginals x)) predicate-amb-list))
 
 (define get-predicate-marginals (predicate candidate-array)
   (array-map (lambda (x) (let ((marginals (get-marginals (predicate x)))
-			       (ind (find-index #t (car marginals))))
-			   (if (= ind -1)
-			       (list 0.0 x)
-			       (list (get-ith-element (cadr marginals) ind) x ))))
-	     candidate-array))
+                               (ind (find-index #t (car marginals))))
+                           (if (= ind -1)
+                             (list 0.0 x)
+                             (list (get-ith-element (cadr marginals) ind) x ))))
+             candidate-array))
 
 (define print-predicate-marginals (predicate candidate-array)
-  (begin 
+  (begin
     (array-map (lambda (x) (new-fg-scope (let ((marginals (get-marginals (predicate x)))
-					       (ind (find-index #t (car marginals))))
-					   (if (= ind -1)
-;(display 0.0 x)
-					       #t 
-					       (let ((prob (get-ith-element (cadr marginals) ind)))
-						 (if (= prob 0.0)
-						     #t
-						   (display prob x )))))))
-	     candidate-array) 
+                                               (ind (find-index #t (car marginals))))
+                                           (if (= ind -1)
+                                             ;(display 0.0 x)
+                                             #t
+                                             (let ((prob (get-ith-element (cadr marginals) ind)))
+                                               (if (= prob 0.0)
+                                                 #t
+                                                 (display prob x )))))))
+               candidate-array)
     #t))
 
 (define print-relation-marginals (predicate candidate-array)
-  (begin 
+  (begin
     (array-map (lambda (x) (let ((marginals (get-marginals (predicate (car x) (cadr x))))
-				 (ind (find-index #t (car marginals))))
-			     (if (= ind -1)
-				 (display 0.0 x)
-				 (display (get-ith-element (cadr marginals) ind) x ))))
-	     candidate-array)
+                                 (ind (find-index #t (car marginals))))
+                             (if (= ind -1)
+                               (display 0.0 x)
+                               (display (get-ith-element (cadr marginals) ind) x ))))
+               candidate-array)
     #t))
 
 
 (define get-related-entities (entity)
   (if (dictionary-contains entity entities)
-      (array-get-ith-element related-entities (dictionary-lookup entity entities))
-      (array )
-      )
-)
+    (array-get-ith-element related-entities (dictionary-lookup entity entities))
+    (array )
+    )
+  )
 
 (define get-all-related-entities (entity-list)
   (if (= 0 (length entity-list))
-      entity-array
-      (foldr array-merge-sets (map get-related-entities entity-list) (array ))))
+    entity-array
+    (foldr array-merge-sets (map get-related-entities entity-list) (array ))))
 
 ;; Trainable functions ;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -119,13 +119,13 @@
 ;; a particular word / entity.
 (define get-params (element element-list parameter-list unknown-elt)
   (if (dictionary-contains element element-list)
-      (get-ith-parameter parameter-list (dictionary-lookup element element-list))
-      (get-ith-parameter parameter-list (dictionary-lookup unknown-elt element-list))))
+    (get-ith-parameter parameter-list (dictionary-lookup element element-list))
+    (get-ith-parameter parameter-list (dictionary-lookup unknown-elt element-list))))
 
 (define get-index-with-unknown (word word-dict)
   (if (dictionary-contains word word-dict)
-      (dictionary-lookup word word-dict)
-      (dictionary-lookup UNKNOWN-WORD word-dict)))
+    (dictionary-lookup word word-dict)
+    (dictionary-lookup UNKNOWN-WORD word-dict)))
 
 (define get-cat-word-params (word word-parameter-list)
   (get-params word cat-words word-parameter-list UNKNOWN-WORD))
@@ -143,24 +143,24 @@
   (lambda (word)
     (lambda (entity)
       (if (dictionary-contains entity entities)
-	  (let ((var (make-entity-var entity)))
-	    (make-inner-product-classifier 
-	     var #t (get-cat-word-params word word-parameters) (get-entity-params entity entity-parameters))
-	    var)
-	  #f)
+        (let ((var (make-entity-var entity)))
+          (make-inner-product-classifier
+            var #t (get-cat-word-params word word-parameters) (get-entity-params entity entity-parameters))
+          var)
+        #f)
       )))
 
 (define word-rel-family (word-rel-params entity-tuple-params)
   (define word-rel (word)
     (lambda (entity1 entity2)
       (if (dictionary-contains (list entity1 entity2) entity-tuples)
-	  (let ((var (make-entity-var (cons entity1 entity2))))
-	    (make-inner-product-classifier 
-	     var #t (get-rel-word-params word word-rel-params)
-	     (get-entity-tuple-params entity1 entity2 entity-tuple-params))
-	    var)
-	  #f
-	  )
+        (let ((var (make-entity-var (cons entity1 entity2))))
+          (make-inner-product-classifier
+            var #t (get-rel-word-params word word-rel-params)
+            (get-entity-tuple-params entity1 entity2 entity-tuple-params))
+          var)
+        #f
+        )
       ))
   word-rel)
 
@@ -180,31 +180,31 @@
         (entity-parameters (get-ith-parameter parameters 1))
         (word-rel-parameters (get-ith-parameter parameters 2))
         (entity-tuple-parameters (get-ith-parameter parameters 3)))
-; (array-map (lambda (word) (display word (parameters-to-string (get-cat-word-params word word-parameters))))
-;	       (dictionary-to-array cat-words))
+    ; (array-map (lambda (word) (display word (parameters-to-string (get-cat-word-params word word-parameters))))
+    ;           (dictionary-to-array cat-words))
 
     (array-map (lambda (word) (display word (parameters-to-string (get-rel-word-params word word-rel-parameters))))
-	       (dictionary-to-array rel-words))
-))
+               (dictionary-to-array rel-words))
+    ))
 
 ;; (display "exists x st. (plano x) and (city x)?")
 ;; (display (get-marginals (expression-eval (quote (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x))) entities)))))
-;; 
+;;
 ;; (display "exists x,y st. (plano x) and (city x) and (texas y) and (in x y)?")
-;; (display (get-marginals (expression-eval (quote (exists-func (lambda (y) (and ((mention "/en/Texas") y) 
-;;                                              (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x)) ((word-rel "in") x y)) entities))) 
+;; (display (get-marginals (expression-eval (quote (exists-func (lambda (y) (and ((mention "/en/Texas") y)
+;;                                              (exists-func (lambda (x) (and ((mention "/en/plano") x) ((word-cat "city") x)) ((word-rel "in") x y)) entities)))
 ;;                             entities)))))
-;; 
-;; 
+;;
+;;
 ;; (display "exists x st. (city x)")
 ;; (display (get-marginals (expression-eval (quote (and (exists-func (word-cat "city") entities))))))
-;; 
-;; 
+;;
+;;
 ;; (display "exists x st. (city x) and exists y st. (city y)")
 ;; (display (get-marginals (expression-eval (quote (and (exists-func (word-cat "city") entities)
 ;;                                                      (exists-func (word-cat "city") entities) )))))
-;; 
-;; 
+;;
+;;
 ;; (display "lambda x. st (city x) (in x y) (texas y)")
 ;; (display entities)
 ;; (display (get-best-value (expression-eval (quote (predicate-to-set (lambda (x) (and ((word-cat "city") x)
