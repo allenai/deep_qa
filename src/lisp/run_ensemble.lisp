a217e649eaec417123d0e83124299304bdd87ea0
Matt Gardner <mattg@allenai.org>
2016-01-04 10:58:35 -0800

    More lisp formatting
    Former-commit-id: 35507ecd85595246839c8e611c7991984b6ab080
    Former-commit-id: 83e0235fc0c5a04f9c523ea31e85ca98bd050284

--- src/lisp/run_ensemble.lisp
+++ src/lisp/run_ensemble.lisp
@@ -7,12 +7,12 @@
 
 (define get-cluster (word word-cluster-dict word-cluster-names cluster-dict clusters)
   (if (dictionary-contains word word-cluster-dict)
-      (array-get-ith-element clusters 
-			     (dictionary-lookup (array-get-ith-element word-cluster-names 
-								       (dictionary-lookup word word-cluster-dict)) cluster-dict))
-      (array-get-ith-element clusters 
-			     (dictionary-lookup (array-get-ith-element word-cluster-names 
-								       (dictionary-lookup UNKNOWN-WORD word-cluster-dict)) cluster-dict))))
+    (array-get-ith-element clusters
+                           (dictionary-lookup (array-get-ith-element word-cluster-names
+                                                                     (dictionary-lookup word word-cluster-dict)) cluster-dict))
+    (array-get-ith-element clusters
+                           (dictionary-lookup (array-get-ith-element word-cluster-names
+                                                                     (dictionary-lookup UNKNOWN-WORD word-cluster-dict)) cluster-dict))))
 
 (define expression-eval (expr)
   (let ((word-parameters (get-ith-parameter best-params 0))
@@ -21,39 +21,39 @@
         (entity-tuple-parameters (get-ith-parameter best-params 3))
         (us-word-cat (word-family word-parameters entity-parameters))
         (us-word-rel (word-rel-family word-rel-parameters entity-tuple-parameters)))
-  
+
     (define word-cat (word)
       (lambda (entity)
-	(let ((baseline-retval (dictionary-contains entity (get-cluster word cat-word-dict cat-word-cluster-names cat-cluster-dict cat-clusters)))
-	      (us-retval ((us-word-cat word) entity))
-	      (retval (amb true-false))
-	      (choose-baseline (amb (list #t #f) (list BASELINE-PROB (- 1.0 BASELINE-PROB))))
-	      )
+        (let ((baseline-retval (dictionary-contains entity (get-cluster word cat-word-dict cat-word-cluster-names cat-cluster-dict cat-clusters)))
+              (us-retval ((us-word-cat word) entity))
+              (retval (amb true-false))
+              (choose-baseline (amb (list #t #f) (list BASELINE-PROB (- 1.0 BASELINE-PROB))))
+              )
 
-	  (require (or choose-baseline (= retval us-retval)))
-	  (require (or (not choose-baseline) (= retval baseline-retval)))
-	  retval
-      )))
+          (require (or choose-baseline (= retval us-retval)))
+          (require (or (not choose-baseline) (= retval baseline-retval)))
+          retval
+          )))
 
     (define word-rel (word)
       (lambda (entity1 entity2)
-	(let ((baseline-retval (dictionary-contains (list entity1 entity2) (get-cluster word rel-word-dict rel-word-cluster-names rel-cluster-dict rel-clusters)))
-	      (us-retval ((us-word-rel word) entity1 entity2))
-	      (retval (amb true-false))
-	      (choose-baseline (amb (list #t #f) (list BASELINE-PROB (- 1.0 BASELINE-PROB))))
-	      )
+        (let ((baseline-retval (dictionary-contains (list entity1 entity2) (get-cluster word rel-word-dict rel-word-cluster-names rel-cluster-dict rel-clusters)))
+              (us-retval ((us-word-rel word) entity1 entity2))
+              (retval (amb true-false))
+              (choose-baseline (amb (list #t #f) (list BASELINE-PROB (- 1.0 BASELINE-PROB))))
+              )
 
-	  (require (or choose-baseline (= retval us-retval)))
-	  (require (or (not choose-baseline) (= retval baseline-retval)))
-	  retval
-	  )))
+          (require (or choose-baseline (= retval us-retval)))
+          (require (or (not choose-baseline) (= retval baseline-retval)))
+          retval
+          )))
 
     (define entities (dictionary-to-array entities))
 
     (eval expr)))
 
-  
 
-      
-    
+
+
+
 
