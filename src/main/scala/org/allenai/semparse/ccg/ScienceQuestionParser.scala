b5701d60ef2977be5b314074bd5106d08fff92f9
Matt Gardner <mattg@allenai.org>
2016-02-13 00:42:18 +0000

    Started (very early) looking at using EasySRL for parsing the science questions
    Former-commit-id: 6c01bd2e21f10d798f1e8805b1f6e77a30efcc69
    Former-commit-id: eda91044a10988e108ed695e537550df1876e9b5

---
+++ src/main/scala/org/allenai/semparse/ccg/ScienceQuestionParser.scala
@@ -0,0 +1,9 @@
+package org.allenai.semparse.ccg
+
+import edu.uw.easysrl.syntax.parser.Parser
+
+class ScienceQuestionParser {
+  val sentenceModelDir = "models/easysrl_sentences/"
+  val questionModelDir = "models/easysrl_questions/"
+
+}
