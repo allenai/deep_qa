ef5242b78b915773cf7d77f00561db49ce5ef2fa
Matt Gardner <mattg@allenai.org>
2016-02-19 19:25:42 +0000

    Moved the initial SFE feature computation locally
    So you don't have to checkout and run the SFE code to get the features, and you don't have to have
    parallel spec files, and other nonsense.
    
    
    Former-commit-id: 7fa94c9fce02654ce42aad9496088f32ce9c2f52
    Former-commit-id: ee49dabd3aeb0ddb3af0eb0bc45daa7dcbe818e5

---
+++ src/main/scala/org/allenai/semparse/PreFilteredFeatureComputer.scala
@@ -0,0 +1,120 @@
+package org.allenai.semparse
+
+import edu.cmu.ml.rtw.pra.experiments.Outputter
+import edu.cmu.ml.rtw.pra.experiments.RelationMetadata
+import edu.cmu.ml.rtw.pra.data.NodeInstance
+import edu.cmu.ml.rtw.pra.data.NodePairInstance
+import edu.cmu.ml.rtw.pra.features.NodePairSubgraphFeatureGenerator
+import edu.cmu.ml.rtw.pra.features.NodeSubgraphFeatureGenerator
+import edu.cmu.ml.rtw.pra.graphs.Graph
+import edu.cmu.ml.rtw.pra.graphs.Graph
+import edu.cmu.ml.rtw.users.matt.util.FileUtil
+import edu.cmu.ml.rtw.users.matt.util.SpecFileReader
+
+import org.json4s._
+
+import scala.collection.mutable
+
+// This class is very similar to SfeFeatureComputer, but here instead of computing (filtered)
+// features and feature vectors to be used during the training and testing of the semantic parser,
+// we are computing full feature vectors that will be used as input to the PMI pipeline that
+// selects features for each word.  So we don't need nearly the complexity that is in
+// SfeFeatureComputer.
+class PreFilteredFeatureComputer(
+  specFile: String,
+  fileUtil: FileUtil = new FileUtil
+) {
+  implicit val formats = DefaultFormats
+  val params = new SpecFileReader("/dev/null").readSpecFile(specFile)
+
+  // Here we need to set up some stuff to use the SFE code.  The first few things don't matter, but
+  // we'll use the graph and feature generators in the code below.
+  val praBase = "/dev/null"
+  val relation = "relation doesn't matter"
+  val outputter = Outputter.justLogger
+  val relationMetadata = new RelationMetadata(JNothing, praBase, outputter, fileUtil)
+  val graph = Graph.create(params \ "graph", praBase, outputter, fileUtil).get
+  val nodeFeatureGenerator = new NodeSubgraphFeatureGenerator(
+    params \ "node features",
+    relation,
+    relationMetadata,
+    outputter,
+    fileUtil
+  )
+  val nodePairFeatureGenerator = new NodePairSubgraphFeatureGenerator(
+    params \ "node pair features",
+    relation,
+    relationMetadata,
+    outputter,
+    fileUtil
+  )
+
+  def computeMidFeatures(mid: String): Seq[String] = {
+    if (graph.hasNode(mid)) {
+      val instance = new NodeInstance(graph.getNodeIndex(mid), true, graph)
+      val subgraph = nodeFeatureGenerator.getLocalSubgraph(instance)
+      nodeFeatureGenerator.extractFeaturesAsStrings(instance, subgraph)
+    } else {
+      Seq()
+    }
+  }
+
+  def computeMidPairFeatures(mid1: String, mid2: String): Seq[String] = {
+    if (graph.hasNode(mid1) && graph.hasNode(mid2)) {
+      val instance = new NodePairInstance(graph.getNodeIndex(mid1), graph.getNodeIndex(mid2), true, graph)
+      val subgraph = nodePairFeatureGenerator.getLocalSubgraph(instance)
+      nodePairFeatureGenerator.extractFeaturesAsStrings(instance, subgraph)
+    } else {
+      Seq()
+    }
+  }
+}
+
+object compute_full_feature_vectors {
+  val dataSize = "small"
+  val midFile = "data/${dataSize}/training-mids.tsv"
+  val midPairFile = "data/${dataSize}/training-mid-pairs.tsv"
+  val midFeatureFile = "data/${dataSize}/pre-filtered-mid-features.tsv"
+  val midPairFeatureFile = "data/${dataSize}/pre-filtered-mid-pair-features.tsv"
+  val specFile = "src/main/resources/sfe_spec_${dataSize}.json"
+
+  def main(args: Array[String]) {
+    val fileUtil = new FileUtil
+    val computer = new PreFilteredFeatureComputer(specFile, fileUtil)
+
+    val midWriter = fileUtil.getFileWriter(midFeatureFile)
+    fileUtil.parProcessFileInChunks(midFile, (lines: Seq[String]) => {
+      val midFeatures = lines.map(line => {
+        val mid = line
+        val features = computer.computeMidFeatures(mid)
+        val featureStr = features.mkString(" -#- ")
+        s"${mid}\t${featureStr}\n"
+      })
+      midWriter synchronized {
+        for (midFeatureLine <- midFeatures) {
+          midWriter.write(midFeatureLine)
+        }
+      }
+    })
+    midWriter.close()
+
+    val midPairWriter = fileUtil.getFileWriter(midPairFeatureFile)
+    fileUtil.parProcessFileInChunks(midPairFile, (lines: Seq[String]) => {
+      val midPairFeatures = lines.map(line => {
+        val midPair = line
+        val midPairFields = midPair.split(" ")
+        val mid1 = midPairFields(0)
+        val mid2 = midPairFields(1)
+        val features = computer.computeMidPairFeatures(mid1, mid2)
+        val featureStr = features.mkString(" -#- ")
+        s"${midPair}\t${featureStr}\n"
+      })
+      midPairWriter synchronized {
+        for (midPairFeatureLine <- midPairFeatures) {
+          midPairWriter.write(midPairFeatureLine)
+        }
+      }
+    })
+    midPairWriter.close()
+  }
+}
