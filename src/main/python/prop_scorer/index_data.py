a2c49331814b3aa5f3af5a8d626bacee421ae41f
Pradeep Dasigi <pradeepd@allenai.org>
2016-06-06 14:51:22 -0700

    Serialize model and properly deal with OOV
    Former-commit-id: c88b744b2f2e329a05d7003c5279c0f3bad6c987
    Former-commit-id: 4469576123fd88a82e292678990f928daa854686

--- src/main/python/prop_scorer/index_data.py
+++ src/main/python/prop_scorer/index_data.py
@@ -3,16 +3,20 @@ from nltk.tokenize import word_tokenize
 
 class DataIndexer(object):
     def __init__(self):
-        self.word_index = {"PADDING":0}
+        self.word_index = {"PADDING":0, "UNK":1}
+        self.singletons = set([])
+        self.non_singletons = set([])
 
-    def get_indices(self, lines, pad=True, separate_propositions=True):
+    def get_indices(self, lines, pad=True, separate_propositions=True, for_train=True):
         '''
         lines: list(str). Sequence of proposition strings. If propositions have to be grouped (to keep track of which sentences they come from), separate props in a line with ';'
         pad: bool. Left pad index vectors to make them all the same size?
-        separate_propositions: Use ';' as a proposition delimiter?
+        separate_propositions: bool. Use ';' as a proposition delimiter?
+        for_train: bool. Setting this to False will treat new words as OOV. Setting it to True will add them to the index if they occur atleast twice. Singletons will always be treated as OOV.
         '''
-        proposition_indices = []
+        all_proposition_indices = []
         num_propositions_in_lines = []
+        all_proposition_words = []
         for line in lines:
             if separate_propositions:
                 line_propositions = line.split(";")
@@ -21,14 +25,31 @@ class DataIndexer(object):
             num_propositions_in_lines.append(len(line_propositions))
             for proposition in line_propositions:
                 words = word_tokenize(proposition.lower())
-                for word in words:
-                    if word not in self.word_index:
-                        self.word_index[word] = len(self.word_index)
-                proposition_indices.append([self.word_index[word] for word in words])
+                all_proposition_words.append(words)
+                if for_train:
+                    for word in words:
+                        if word not in self.non_singletons:
+                            if word in self.singletons:
+                                # Since we are seeing the word again, it is not a singleton
+                                self.singletons.remove(word)
+                                self.non_singletons.add(word)
+                            else:
+                                # We have not seen this word. It is a singleton (atleast for now)
+                                self.singletons.add(word)
+        if for_train:
+            # Add non-singletons to index. The singletons will remain unknown.
+            for word in self.non_singletons:
+                if word not in self.word_index:
+                    self.word_index[word] = len(self.word_index)
+
+        for words in all_proposition_words:
+            proposition_indices = [self.word_index[word] if word in self.word_index else self.word_index["UNK"] for word in words]
+            all_proposition_indices.append(proposition_indices)
+
         if pad:
-            return num_propositions_in_lines, self.pad_indices(proposition_indices)
+            return num_propositions_in_lines, self.pad_indices(all_proposition_indices)
         else:
-            return num_propositions_in_lines, proposition_indices
+            return num_propositions_in_lines, all_proposition_indices
 
     def pad_indices(self, all_indices, max_length=None):
         if max_length is None:
@@ -50,10 +71,16 @@ class DataIndexer(object):
             if token in self.word_index:
                 indices_to_ignore.add(self.word_index[token])
         for indices in all_indices:
+            first_non_zero_index = 0
+            for index in indices:
+                if index == 0:
+                    first_non_zero_index += 1
+                else:
+                    break
             corrupted_indices = list(indices)
             num_corrupted_locations = 0
             while num_corrupted_locations < num_locations_to_corrupt:
-                rand_location = random.randint(0, len(indices)-1)
+                rand_location = random.randint(first_non_zero_index, len(indices)-1)
                 if corrupted_indices[rand_location] in indices_to_ignore:
                     continue
                 rand_index = 0
--- src/main/python/prop_scorer/prop_scorer.py
+++ src/main/python/prop_scorer/prop_scorer.py
@@ -2,8 +2,9 @@ import sys
 import argparse
 
 import numpy
+import pickle
 from keras.layers import Embedding, Input, LSTM, Dense, merge
-from keras.models import Model
+from keras.models import Model, model_from_yaml
 from keras import backend as K
 
 from index_data import DataIndexer
@@ -11,6 +12,7 @@ from index_data import DataIndexer
 class PropScorer(object):
     def __init__(self):
         self.model = None
+        self.data_indexer = DataIndexer()
 
     def train(self, good_input, bad_input, embedding_size=50, vocab_size=None):
         '''
@@ -20,7 +22,7 @@ class PropScorer(object):
         vocab_size: int. Input dimensionality of embedding layer. Will be inferred from inputs if not provided.
         '''
         if vocab_size is None:
-            vocab_size = max(good_input.max(), bad_input.max()) + 1
+            vocab_size = len(self.data_indexer.word_index) + 1
 
         ## STEP 1: Initialze the two inputs
         good_input_layer = Input(shape=good_input.shape[1:], dtype='int32')
@@ -53,7 +55,7 @@ class PropScorer(object):
         score_hinge_loss = lambda dummy_target, diff: K.mean(diff + 0*dummy_target, axis=-1)
         model = Model(input=[good_input_layer, bad_input_layer], output=score_diff)
         model.compile(loss=score_hinge_loss, optimizer='rmsprop')
-        model.fit([good_input, bad_input], numpy.zeros((good_input.shape[:1]))) # Note: target is ignored by loss. See above.
+        model.fit([good_input, bad_input], numpy.zeros((good_input.shape[:1])), validation_split=0.1) # Note: target is ignored by loss. See above.
         self.model = model
 
         ## Step 6: Define a scoring model taking the necessary parts of the trained model
@@ -65,52 +67,93 @@ class PropScorer(object):
         # We just need the model to predict. The loss and optimizer do not matter since we are not going to train.
         self.scoring_model.compile(loss='mse', optimizer='sgd')
 
+    def save_model(self, model_name_prefix="prop_scorer"):
+        # Serializing the scoring model for future use.
+        # Do we need to save the original model as well?
+        model_config = self.scoring_model.to_yaml()
+        model_config_file = open("%s_config.yaml"%(model_name_prefix), "w")
+        print >>model_config_file, model_config
+        self.scoring_model.save_weights("%s_weights.h5"%model_name_prefix, overwrite=True)
+        data_indexer_file = open("%s_data_indexer.pkl"%model_name_prefix, "w")
+        pickle.dump(self.data_indexer, data_indexer_file)
+
+    def load_model(self, model_name_prefix="prop_scorer"):
+        # Loading serialized model
+        model_config_file = "%s_config.yaml"%(model_name_prefix)
+        model_config_yaml = open(model_config_file).read()
+        self.scoring_model = model_from_yaml(model_config_yaml)
+        self.scoring_model.load_weights("%s_weights.h5"%model_name_prefix)
+        data_indexer_file = open("%s_data_indexer.pkl"%model_name_prefix)
+        self.data_indexer = pickle.load(data_indexer_file)
+        self.scoring_model.compile(loss='mse', optimizer='sgd')
+
+    def index_data(self, data_lines, for_train=True, max_length=None):
+        if for_train:
+            # Indexing training data
+            train_lengths, good_input = self.data_indexer.get_indices(data_lines, separate_propositions=False, for_train=for_train)
+            # Padding to prespecified length
+            print >>sys.stderr, "Indexing training data"
+            good_input = self.data_indexer.pad_indices(good_input, max_length=max_length)
+
+            # Corrupting train indices to bet "bad" data
+            print >>sys.stderr, "Corrupting training data"
+            bad_input = self.data_indexer.corrupt_indices(good_input)
+
+            # Make them both int32 arrays so that Keras will view them as indices.
+            good_input = numpy.asarray(good_input, dtype='int32')
+            bad_input = numpy.asarray(bad_input, dtype='int32')
+            return train_lengths, (good_input, bad_input)
+        else:
+            # Indexing test data
+            print >>sys.stderr, "Indexing test data"
+            test_lengths, test_indices = self.data_indexer.get_indices(test_lines, for_train=for_train)
+            test_indices = self.data_indexer.pad_indices(test_indices, max_length=max_length)
+            test_indices = numpy.asarray(test_indices, dtype='int32')
+            return test_lengths, (test_indices,)
+        
     def score(self, input):
         score_val = self.scoring_model.predict(input)
         return score_val
 
 if __name__=="__main__":
     argparser = argparse.ArgumentParser(description="Simple proposition scorer")
-    argparser.add_argument('train_file', type=str)
-    argparser.add_argument('test_file', type=str)
+    argparser.add_argument('--train_file', type=str)
+    argparser.add_argument('--test_file', type=str)
     args = argparser.parse_args()
-    lines = [x.strip() for x in open(args.train_file).readlines()]
-    test_lines = [x.strip() for x in open(args.test_file).readlines()]
-    data_indexer = DataIndexer()
-    
-    # Indexing training data
-    _, good_input = data_indexer.get_indices(lines, separate_propositions=False)
-    
-    # Indexing test data
-    test_lengths, test_indices = data_indexer.get_indices(test_lines)
-    
-    # Pad both the indices to make sure that the time dimension is of the same length
-    max_length = max(len(good_input[0]), len(test_indices[0]))
-    good_input = data_indexer.pad_indices(good_input, max_length=max_length)
-    test_indices = data_indexer.pad_indices(test_indices, max_length=max_length)
-    
-    # Corrupting train indices to bet "bad" data
-    bad_input = data_indexer.corrupt_indices(good_input)
-
-    good_input = numpy.asarray(good_input, dtype='int32')
-    bad_input = numpy.asarray(bad_input, dtype='int32')
-    test_indices = numpy.asarray(test_indices, dtype='int32')
-    vocab_size = max(good_input.max(), bad_input.max(), test_indices.max()) + 1
     prop_scorer = PropScorer()
-    prop_scorer.train(good_input, bad_input, vocab_size=vocab_size)
-    test_all_prop_scores = prop_scorer.score(test_indices)
-    test_scores = []
-    t_ind = 0
 
-    # Iterating through all propositions in the sentence and aggregating their scores
-    for num_propositions in test_lengths:
-        test_scores.append(test_all_prop_scores[t_ind:t_ind+num_propositions].sum())
-        t_ind += num_propositions
+    if not args.train_file:
+        # Training file is not given. There must be a serialized model.
+        print >>sys.stderr, "Loading scoring model from disk"
+        prop_scorer.load_model()
+        # input shape of scoring model is (samples, max_length, word_dim)
+        max_length = prop_scorer.scoring_model.get_input_shape_at(0)[1] 
+    else:
+        print >>sys.stderr, "Reading training data"
+        lines = [x.strip() for x in open(args.train_file).readlines()]
+        max_length = max([len(line) for line in lines])
+        _, (good_input, bad_input) = prop_scorer.index_data(lines, for_train=True, max_length=max_length)
+        print >>sys.stderr, "Training model"
+        prop_scorer.train(good_input, bad_input)
+        prop_scorer.save_model()
 
-    # Once aggregated, the number of scores should be the same as test sentences.
-    assert len(test_scores) == len(test_lines)
+    if args.test_file is not None:
+        test_lines = [x.strip() for x in open(args.test_file).readlines()]
+        test_lengths, (test_indices,) = prop_scorer.index_data(test_lines, max_length=max_length, for_train=False)
+        print >>sys.stderr, "Scoring test data"
+        test_all_prop_scores = prop_scorer.score(test_indices)
+        test_scores = []
+        t_ind = 0
 
-    outfile = open("out.txt", "w")
-    for score, line in zip(test_scores, test_lines):
-        print >>outfile, score, line
-    outfile.close()
+        # Iterating through all propositions in the sentence and aggregating their scores
+        for num_propositions in test_lengths:
+            test_scores.append(test_all_prop_scores[t_ind:t_ind+num_propositions].sum())
+            t_ind += num_propositions
+
+        # Once aggregated, the number of scores should be the same as test sentences.
+        assert len(test_scores) == len(test_lines)
+
+        outfile = open("out.txt", "w")
+        for score, line in zip(test_scores, test_lines):
+            print >>outfile, score, line
+        outfile.close()
