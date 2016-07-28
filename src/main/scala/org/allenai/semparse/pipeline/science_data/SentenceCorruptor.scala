cba606a69e8830030477f2667faf85d2106f46f4
Matt Gardner <mattg@allenai.org>
2016-07-28 22:21:55 +0000

    Fixed some sentence-producer issues
    Now QuestionInterpreter is a SentenceProducer, which makes getting background sentences for
    questions a whole lot easier.
    
    
    Former-commit-id: bd93f34774e9fa242b2b8a9c4d40ab8f160de2fd
    Former-commit-id: 2cc885be68e7a8195ddd75758e6f38f7c1326207

--- src/main/scala/org/allenai/semparse/pipeline/science_data/SentenceCorruptor.scala
+++ src/main/scala/org/allenai/semparse/pipeline/science_data/SentenceCorruptor.scala
@@ -20,10 +20,49 @@ import com.mattg.util.JsonHelper
  * same as input format: either "[sentence]" or "[index][tab][sentence]".  Note that the indices
  * are not guaranteed to match between input good statement and output bad statement.  If we
  * actually need that at some point, I'll add it.
- * TODO(matt): the python code doesn't actually match this input or output spec yet.  There are
- * no indices.  Also, this is the input/output spec for SentenceCorrupter, not
- * SentenceCorruptorTrainer...
  */
+class SentenceCorruptor(
+  val params: JValue,
+  val fileUtil: FileUtil
+) extends SubprocessStep(Some(params), fileUtil) with SentenceProducer {
+  implicit val formats = DefaultFormats
+  override val name = "Sentence Corruptor"
+
+  val validParams = baseParams ++ Seq("positive data", "trainer")
+  JsonHelper.ensureNoExtras(params, name, validParams)
+
+  val trainer = new SentenceCorruptorTrainer(params \ "trainer", fileUtil)
+  val factorBase = JsonHelper.extractWithDefault(params, "factor base", 2)
+  val tokenizeInput = JsonHelper.extractWithDefault(params, "tokenize input", true)
+  val tokenizeInputArg = if (tokenizeInput) Seq() else Seq("--no_tokenize")
+  val useLstm = JsonHelper.extractWithDefault(params, "use lstm", false)
+  val useLstmArg = if (useLstm) Seq("--use_lstm") else Seq()
+  val trainingEpochs = JsonHelper.extractWithDefault(params, "max training epochs", 20)
+  val maxSentencesArgs = maxSentences.map(max => Seq("--max_corrupted_instances", max.toString)).toSeq.flatten
+  val indexSentencesArg = if (indexSentences) Seq("--create_sentence_indices") else Seq()
+
+  val sentenceProducer = SentenceProducer.create(params \ "positive data", fileUtil)
+  val positiveDataFile = sentenceProducer.outputFile
+  override val outputFile = positiveDataFile.dropRight(4) + "_corrupted.tsv"
+
+  override val binary = "python"
+  override val scriptFile = Some("src/main/python/sentence_corruption/lexical_substitution.py")
+  override val arguments = Seq(
+    "--test_file", positiveDataFile,
+    "--output_file", outputFile,
+    "--word_dim", trainer.wordDimensionality.toString,
+    "--factor_base", trainer.factorBase.toString,
+    "--model_serialization_prefix", trainer.modelPrefix
+  ) ++ tokenizeInputArg ++ useLstmArg ++ maxSentencesArgs ++ indexSentencesArg
+
+  override val inputs: Set[(String, Option[Step])] = Set(
+    (positiveDataFile, Some(sentenceProducer))
+  ) ++ trainer.outputs.map(file => (file, Some(trainer)))
+  override val outputs = Set(outputFile)
+  override val paramFile = outputFile.dropRight(4) + "_params.json"
+  override val inProgressFile = outputFile.dropRight(4) + "_in_progress"
+}
+
 class SentenceCorruptorTrainer(
   params: JValue,
   fileUtil: FileUtil
@@ -67,45 +106,4 @@ class SentenceCorruptorTrainer(
   )
   override val paramFile = modelPrefix + "_params.json"
   override val inProgressFile = modelPrefix + "_in_progress"
-}
-
-class SentenceCorruptor(
-  val params: JValue,
-  val fileUtil: FileUtil
-) extends SubprocessStep(Some(params), fileUtil) with SentenceProducer {
-  implicit val formats = DefaultFormats
-  override val name = "Sentence Corruptor"
-
-  val validParams = baseParams ++ Seq("positive data", "trainer")
-  JsonHelper.ensureNoExtras(params, name, validParams)
-
-  val trainer = new SentenceCorruptorTrainer(params \ "trainer", fileUtil)
-  val factorBase = JsonHelper.extractWithDefault(params, "factor base", 2)
-  val tokenizeInput = JsonHelper.extractWithDefault(params, "tokenize input", true)
-  val tokenizeInputArg = if (tokenizeInput) Seq() else Seq("--no_tokenize")
-  val useLstm = JsonHelper.extractWithDefault(params, "use lstm", false)
-  val useLstmArg = if (useLstm) Seq("--use_lstm") else Seq()
-  val trainingEpochs = JsonHelper.extractWithDefault(params, "max training epochs", 20)
-  val maxSentencesArgs = maxSentences.map(max => Seq("--max_corrupted_instances", max.toString)).toSeq.flatten
-
-  val sentenceProducer = SentenceProducer.create(params \ "positive data", fileUtil)
-  val positiveDataFile = sentenceProducer.outputFile
-  override val outputFile = positiveDataFile.dropRight(4) + "_corrupted.tsv"
-
-  override val binary = "python"
-  override val scriptFile = Some("src/main/python/sentence_corruption/lexical_substitution.py")
-  override val arguments = Seq(
-    "--test_file", positiveDataFile,
-    "--output_file", outputFile,
-    "--word_dim", trainer.wordDimensionality.toString,
-    "--factor_base", trainer.factorBase.toString,
-    "--model_serialization_prefix", trainer.modelPrefix
-  ) ++ tokenizeInputArg ++ useLstmArg  // TODO(matt): ++ maxSentencesArgs
-
-  override val inputs: Set[(String, Option[Step])] = Set(
-    (positiveDataFile, Some(sentenceProducer))
-  ) ++ trainer.outputs.map(file => (file, Some(trainer)))
-  override val outputs = Set(outputFile)
-  override val paramFile = outputFile.dropRight(4) + "_params.json"
-  override val inProgressFile = outputFile.dropRight(4) + "_in_progress"
 }
