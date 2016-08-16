9d5637c8a3dbd2aaa3881b101808b34014ba07d1
Matt Gardner <mattg@allenai.org>
2016-08-16 21:59:55 +0000

    Adding tests for reading TextInstance from line

--- src/test/python/dlfa/data/dataset_test.py
+++ src/test/python/dlfa/data/dataset_test.py
@@ -1,11 +1,10 @@
-import sys
-print(sys.path)
 from dlfa.data.dataset import Dataset
-from dlfa.data.instance import Instance
+from dlfa.data.instance import TextInstance
 
-def test_merge():
-    instances = [Instance("testing", None, None), Instance("testing1", None, None)]
-    dataset1 = Dataset(instances[:1])
-    dataset2 = Dataset(instances[1:])
-    merged = dataset1.merge(dataset2)
-    assert merged.instances == instances
+class TestDataset:
+    def test_merge(self):
+        instances = [TextInstance("testing", None, None), TextInstance("testing1", None, None)]
+        dataset1 = Dataset(instances[:1])
+        dataset2 = Dataset(instances[1:])
+        merged = dataset1.merge(dataset2)
+        assert merged.instances == instances
---
+++ src/test/python/dlfa/data/instance_test.py
@@ -0,0 +1,75 @@
+from dlfa.data.instance import TextInstance
+
+class TestTextInstance:
+    @staticmethod
+    def instance_to_line(text, label=None, index=None):
+        line = ''
+        if index is not None:
+            line += str(index) + '\t'
+        line += text
+        if label is not None:
+            label_str = '1' if label else '0'
+            line += '\t' + label_str
+        return line
+
+    def test_read_from_line_handles_one_column(self):
+        text = "this is a sentence"
+        instance = TextInstance.read_from_line(text)
+        assert instance.text == text
+        assert instance.label is None
+        assert instance.index is None
+
+    def test_read_from_line_handles_three_column(self):
+        index = 23
+        text = "this is a sentence"
+        label = True
+        line = self.instance_to_line(text, label, index)
+
+        instance = TextInstance.read_from_line(line)
+        assert instance.text == text
+        assert instance.label == label
+        assert instance.index == index
+
+    def test_read_from_line_handles_two_column_with_label(self):
+        index = None
+        text = "this is a sentence"
+        label = True
+        line = self.instance_to_line(text, label, index)
+
+        instance = TextInstance.read_from_line(line)
+        assert instance.text == text
+        assert instance.label == label
+        assert instance.index == index
+
+    def test_read_from_line_handles_two_column_with_index(self):
+        index = 23
+        text = "this is a sentence"
+        label = None
+        line = self.instance_to_line(text, label, index)
+
+        instance = TextInstance.read_from_line(line)
+        assert instance.text == text
+        assert instance.label == label
+        assert instance.index == index
+
+    def test_read_from_line_handles_two_column_with_default_true(self):
+        index = 23
+        text = "this is a sentence"
+        label = None
+        line = self.instance_to_line(text, label, index)
+
+        instance = TextInstance.read_from_line(line, default_label=True)
+        assert instance.text == text
+        assert instance.label == True
+        assert instance.index == index
+
+    def test_read_from_line_handles_two_column_with_default_false(self):
+        index = 23
+        text = "this is a sentence"
+        label = None
+        line = self.instance_to_line(text, label, index)
+
+        instance = TextInstance.read_from_line(line, default_label=False)
+        assert instance.text == text
+        assert instance.label == False
+        assert instance.index == index
