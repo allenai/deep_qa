b57d129f7bac61007d63525c9071f000363028e0
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-08-22 18:46:55 -0400

    Added Mou et al.'s heuristic matching for entailment

--- src/main/python/dlfa/layers/entailment_models.py
+++ src/main/python/dlfa/layers/entailment_models.py
@@ -12,7 +12,7 @@ attended_background_knowledge and current_memory are two different vectors that 
 reasoning over this background knowledge, which the entailment model can choose to use as desired.
 """
 
-from keras.layers import Dense
+from keras.layers import Dense, merge
 
 
 class DenseMemoryOnlyEntailmentModel(object):
@@ -21,7 +21,26 @@ class DenseMemoryOnlyEntailmentModel(object):
         # pylint: disable=unused-argument
         return Dense(output_dim=2, activation='softmax', name='entailment_softmax')(current_memory)
 
+class HeuristicMatchingEntailmentModel(object):
+    """
+    This class is an implementation of the heuristic matching algorithm proposed in the following paper:
+    "Natural Language Inference by Tree-Based Convolution and Heuristic Matching", Mou et al, ACL 2016
+    """
+    @staticmethod
+    def classify(sentence_encoding, current_memory, attended_background_knowledge):
+        # pylint: disable=unused-argument
+        sentence_memory_concat = merge([sentence_encoding, current_memory], mode="concat")
+        sentence_memory_product = merge([sentence_encoding, current_memory], mode="mul")
+        diff = lambda layers: layers[0] - layers[1]  # Keras does not have this merge mode
+        diff_shape = lambda shapes: shapes[0]
+        sentence_memory_diff = merge([sentence_encoding, current_memory], mode=diff,
+                                     output_shape=diff_shape)
+        merged_input = merge([sentence_memory_concat, sentence_memory_product,
+                              sentence_memory_diff], mode="concat")
+        return Dense(output_dim=2, activation='softmax', name='entailment_softmax')(merged_input)
+
 
 entailment_models = {  # pylint: disable=invalid-name
-        'dense_memory_only': DenseMemoryOnlyEntailmentModel
+        'dense_memory_only': DenseMemoryOnlyEntailmentModel,
+        'heuristic_matching': HeuristicMatchingEntailmentModel
         }
