12ea070da8c5fa8a71ece6e4d218c72ada82027f
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-08-22 12:47:59 -0400

    Removed inheritance from object

--- src/main/python/dlfa/layers/memory_updaters.py
+++ src/main/python/dlfa/layers/memory_updaters.py
@@ -9,7 +9,7 @@ from keras import backend as K
 from keras.layers import Dense, merge
 
 
-class SumMemoryUpdater(object):
+class SumMemoryUpdater:
     def __init__(self, output_dim, name="sum_memory_updater"):
         self.output_dim = output_dim
         self.name = name
@@ -17,10 +17,10 @@ class SumMemoryUpdater(object):
     def update(self, memory_vector, aggregated_knowledge_vector):
         # Returning a Keras layer instead of symbolic sum so that we can name the output.
         return merge([memory_vector, aggregated_knowledge_vector], mode='sum',
-                    output_shape=self.output_dim, name=self.name)
+                    output_shape=(self.output_dim,) name=self.name)
 
 
-class DenseConcatMemoryUpdater(object):
+class DenseConcatMemoryUpdater:
     def __init__(self, output_dim, name="dense_concat_memory_updater"):
         self.output_dim = output_dim
         self.name = name
