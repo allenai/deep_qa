e7bce3f8aa8e4b30f36173020baf7c52121d64b2
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-09-21 15:28:55 -0400

    Tensorflow compatible tiling and updated hardmax test

--- src/main/python/dlfa/layers/knowledge_selectors.py
+++ src/main/python/dlfa/layers/knowledge_selectors.py
@@ -26,7 +26,7 @@ def tile_sentence_encoding(sentence_encoding, knowledge_encoding):
 
 def hardmax(unnormalized_attention, knowledge_length):
     # (num_samples, knowledge_length)
-    tiled_max_values = K.transpose(K.tile(K.max(unnormalized_attention, axis=1), (knowledge_length, 1)))
+    tiled_max_values = K.tile(K.expand_dims(K.max(unnormalized_attention, axis=1)), (1, knowledge_length))
     # We now have a matrix where every column in each row has the max knowledge score value from the
     # corresponding row in the unnormalized attention matrix.
     # Next, we will compare that all-max matrix with the original input, resulting in ones where the 
