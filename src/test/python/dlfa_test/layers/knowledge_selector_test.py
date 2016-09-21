b25bfdabf88f137f000dcdda89b8ff72e4211cb0
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2016-09-21 12:59:50 -0400

    Added a test for hardmax

---
+++ src/test/python/dlfa_test/layers/knowledge_selector_test.py
@@ -0,0 +1,20 @@
+import numpy
+from keras import initializations
+from keras import backend as K
+
+from dlfa.layers.knowledge_selectors import hardmax
+
+class TestKnowledgeSelector:
+    def test_hardmax(self):
+        num_samples = 10
+        knowledge_length = 5
+        init = initializations.get('uniform')
+        unnormalized_attention = init((num_samples, knowledge_length))
+        hardmax_output = hardmax(unnormalized_attention, knowledge_length)
+        output_value = K.eval(hardmax_output)
+        assert output_value.shape == (num_samples, knowledge_length)
+        # Assert all elements other than the ones are zeros
+        assert numpy.count_nonzero(output_value) == num_samples
+        # Assert the max values in all rows are ones
+        assert numpy.all(numpy.equal(numpy.max(output_value, axis=0),
+                                     numpy.ones((knowledge_length,))))
