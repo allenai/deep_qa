7cd198cd3097fe543b5a1f6b29692fec3fd88923
Matt Gardner <mattg@allenai.org>
2016-10-05 22:08:27 +0000

    Fixed a small bug in checks.py, added a directory to .gitignore

--- src/main/python/deep_qa/common/checks.py
+++ src/main/python/deep_qa/common/checks.py
@@ -13,7 +13,7 @@ class ConfigurationError(Exception):
 
 def ensure_pythonhashseed_set():
     message = """You must set PYTHONHASHSEED so that we get repeatable results.
-    You can do this with the command `export PYTHONHASHSEED=%d`.
+    You can do this with the command `export PYTHONHASHSEED=%s`.
     See https://docs.python.org/3/using/cmdline.html#envvar-PYTHONHASHSEED for more info.
     """
     assert os.environ.get('PYTHONHASHSEED', None) == PYTHONHASHSEED, message % PYTHONHASHSEED
