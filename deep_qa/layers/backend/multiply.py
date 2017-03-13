2f9831ac06a6453968aa672baae15640ea5e762d
Matt Gardner <mattg@allenai.org>
2017-03-13 10:53:27 -0700

    Made Multiply handle uneven tensors (#252)

--- deep_qa/layers/backend/multiply.py
+++ deep_qa/layers/backend/multiply.py
@@ -10,10 +10,13 @@ class Multiply(Layer):
     can support masking (and because it's slightly nicer to read in a model definition than a
     lambda layer).
 
+    We also try to be a little bit smart if you're wanting to broadcast the multiplication, by
+    having the tensors differ in the number of dimensions by one.
+
     Input:
         - tensor_1: a tensor of arbitrary shape, with an optional mask of the same shape
-        - tensor_2: a tensor with the same shape as ``tensor_1``, with an optional mask of the same
-          shape
+        - tensor_2: a tensor with the same shape as ``tensor_1`` (or one less or one more
+          dimension), with an optional mask of the same shape
 
     Output:
         - ``tensor_1 * tensor_2``.
@@ -31,7 +34,8 @@ class Multiply(Layer):
             tensor_1_mask = K.ones_like(tensor_1)
         if tensor_2_mask is None:
             tensor_2_mask = K.ones_like(tensor_2)
-        return tensor_1_mask * tensor_2_mask
+        tensor_1_mask, tensor_2_mask = self.expand_dims_if_necessary(tensor_1_mask, tensor_2_mask)
+        return K.cast(tensor_1_mask, 'uint8') * K.cast(tensor_2_mask, 'uint8')
 
     @overrides
     def get_output_shape_for(self, input_shape):
@@ -40,4 +44,19 @@ class Multiply(Layer):
     @overrides
     def call(self, x, mask=None):
         tensor_1, tensor_2 = x
+        tensor_1, tensor_2 = self.expand_dims_if_necessary(tensor_1, tensor_2)
         return tensor_1 * tensor_2
+
+    @staticmethod
+    def expand_dims_if_necessary(tensor_1, tensor_2):
+        tensor_1_ndim = K.ndim(tensor_1)
+        tensor_2_ndim = K.ndim(tensor_2)
+        if tensor_1_ndim == tensor_2_ndim:
+            return tensor_1, tensor_2
+        elif tensor_1_ndim == tensor_2_ndim - 1:
+            return K.expand_dims(tensor_1), tensor_2
+        elif tensor_2_ndim == tensor_1_ndim - 1:
+            return tensor_1, K.expand_dims(tensor_2)
+        else:
+            raise RuntimeError("Can't multiply two tensors with ndims "
+                               "{} and {}".format(tensor_1_ndim, tensor_2_ndim))
--- tests/layers/backend/multiply_test.py
+++ tests/layers/backend/multiply_test.py
@@ -30,3 +30,25 @@ class TestMultiply:
                                                             [0, 0, -4, -20, -8]])
         numpy.testing.assert_almost_equal(mask_tensor, [[1, 1, 0, 0, 1],
                                                         [0, 0, 1, 1, 1]])
+
+    def test_call_works_with_uneven_dims(self):
+        batch_size = 1
+        input_length = 2
+        input_length_2 = 5
+        input_1_layer = Input(shape=(input_length, input_length_2), dtype='float32')
+        input_2_layer = Input(shape=(input_length,), dtype='float32')
+        masking_layer = AddMask()
+        masked_input_1 = masking_layer(input_1_layer)
+        masked_input_2 = masking_layer(input_2_layer)
+        multiply_output = Multiply()([masked_input_1, masked_input_2])
+        multiply_mask = OutputMask()(multiply_output)
+        model = Model(input=[input_1_layer, input_2_layer], output=[multiply_output, multiply_mask])
+        input_1_tensor = numpy.asarray([[[2, 5, 0, 1, -4],
+                                         [-1, 0, -2, -10, -4]]])
+        input_2_tensor = numpy.asarray([[2, 1]])
+        multiply_tensor, mask_tensor = model.predict([input_1_tensor, input_2_tensor])
+        assert multiply_tensor.shape == (batch_size, input_length, input_length_2)
+        numpy.testing.assert_almost_equal(multiply_tensor, [[[4, 10, 0, 2, -8],
+                                                             [-1, 0, -2, -10, -4]]])
+        numpy.testing.assert_almost_equal(mask_tensor, [[[1, 1, 0, 1, 1],
+                                                         [1, 0, 1, 1, 1]]])
