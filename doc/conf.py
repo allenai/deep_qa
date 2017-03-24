a46cd666328394bbe4314769d9c353568267cb92
Nelson Liu <nelson-liu@users.noreply.github.com>
2017-03-24 10:45:48 -0700

    Add collapsible code blocks, and use them in model train logs (#273)

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ extensions = ['sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-# templates_path = ['_templates']
+templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -106,7 +106,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 
 # -- Options for HTMLHelp output ------------------------------------------
