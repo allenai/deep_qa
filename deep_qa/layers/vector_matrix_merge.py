b513987f82f6c6c088331eac9c005c7f63835a45
Matt Gardner <mattg@allenai.org>
2017-06-13 09:43:28 -0700

    Make VectorMatrixMerge handle more general cases (#392)
    * Made VectorMatrixMerge handle more general cases
    
    * Minor fixes from PR comments

--- deep_qa/layers/vector_matrix_merge.py
+++ deep_qa/layers/vector_matrix_merge.py
@@ -39,7 +39,7 @@ class VectorMatrixMerge(MaskedLayer):
                  propagate_mask: bool=True,
                  **kwargs):
         self.concat_axis = concat_axis
-        self.mask_concat_axis = mask_concat_axis if mask_concat_axis is not None else concat_axis
+        self.mask_concat_axis = mask_concat_axis
         self.propagate_mask = propagate_mask
         super(VectorMatrixMerge, self).__init__(**kwargs)
 
@@ -47,13 +47,10 @@ class VectorMatrixMerge(MaskedLayer):
     def call(self, inputs, mask=None):
         # We need to reverse these here, so that the order is preserved when we roll out the
         # concatenations.
-        vectors = reversed(inputs[:-1])
+        vectors = inputs[:-1]
         matrix = inputs[-1]
-        result = matrix
-        for vector in vectors:
-            expanded_vector = K.expand_dims(vector, axis=self.concat_axis)
-            result = K.concatenate([expanded_vector, result], axis=self.concat_axis)
-        return result
+        expanded_vectors = [K.expand_dims(vector, axis=self.concat_axis) for vector in vectors]
+        return K.concatenate(expanded_vectors + [matrix], axis=self.concat_axis)
 
     @overrides
     def compute_output_shape(self, input_shapes):
@@ -67,17 +64,33 @@ class VectorMatrixMerge(MaskedLayer):
     def compute_mask(self, inputs, mask=None):
         if mask is None or all(m is None for m in mask) or not self.propagate_mask:
             return None
-        print(mask)
+        mask_concat_axis = self.mask_concat_axis
+        if mask_concat_axis is None:
+            mask_concat_axis = self.concat_axis
+            if mask_concat_axis < 0:
+                mask_concat_axis %= K.ndim(inputs[-1])
         num_vectors = len(mask) - 1
-        matrix_mask = K.cast(mask[-1], 'uint8')
-        # We're just trying to get a mask of the right shape, here, so we can call K.ones_like() on
-        # it.  We'll ignore the actual values in this.
-        vector_mask_template = K.sum(matrix_mask, axis=self.mask_concat_axis)
-        vector_mask = K.expand_dims(K.ones_like(vector_mask_template), axis=self.mask_concat_axis)
-        result_mask = matrix_mask
-        for _ in range(num_vectors):
-            result_mask = K.concatenate([vector_mask, result_mask], axis=self.mask_concat_axis)
-        return K.cast(result_mask, 'bool')
+        matrix_mask = mask[-1]
+        if mask_concat_axis >= K.ndim(matrix_mask):
+            # This means we're concatenating along an axis in the tensor that is greater than the
+            # number of dimensions in the mask.  E.g., we're adding a single pre-computed feature
+            # to a word embedding (if it was multiple features, you'd already have evenly shaped
+            # tensors, so you could just use a Concatenate layer).  In this case, we take all of
+            # the masks, assume they have the same shape, and compute K.any() with them.
+            masks = [matrix_mask] + [m for m in mask[:-1] if m is not None]
+            shapes = set([K.int_shape(m) for m in masks])
+            assert len(shapes) == 1, "Can't compute mask with uneven shapes: " + shapes
+            expanded_masks = [K.expand_dims(m, axis=-1) for m in masks]
+            concated_masks = K.concatenate(expanded_masks, axis=-1)
+            return K.any(concated_masks, axis=-1)
+        vector_masks = []
+        for i in range(num_vectors):
+            vector_mask = mask[i]
+            if vector_mask is None:
+                vector_mask_template = K.sum(K.cast(matrix_mask, 'uint8'), axis=mask_concat_axis)
+                vector_mask = K.cast(K.ones_like(vector_mask_template), 'bool')
+            vector_masks.append(K.expand_dims(vector_mask, axis=mask_concat_axis))
+        return K.concatenate(vector_masks + [matrix_mask], axis=mask_concat_axis)
 
     @overrides
     def get_config(self):
---
+++ tests/layers/vector_matrix_merge_test.py
@@ -0,0 +1,75 @@
+# pylint: disable=no-self-use,invalid-name
+import numpy
+from numpy.testing import assert_array_equal
+
+from keras.layers import Input
+from keras.models import Model
+
+from deep_qa.layers import TimeDistributedEmbedding, VectorMatrixMerge
+from deep_qa.layers.wrappers import OutputMask
+
+class TestVectorMatrixMerge:
+    def test_merge_works_correctly_on_word_indices(self):
+        vocab_size = 10
+        sentence_length = 10
+        word_length = 7
+        num_sentences = 7
+        for concat_axis in [2, -1]:
+            sentence_input = Input(shape=(sentence_length, word_length - 2), dtype='int32')
+            additional_input = Input(shape=(sentence_length,), dtype='int32')
+            additional_input2 = Input(shape=(sentence_length,), dtype='int32')
+            merge_layer = VectorMatrixMerge(concat_axis=concat_axis)
+            merged = merge_layer([additional_input, additional_input2, sentence_input])
+            model = Model(inputs=[sentence_input, additional_input, additional_input2], outputs=merged)
+            sentence_tensor = numpy.random.randint(0, vocab_size, (num_sentences, sentence_length, word_length))
+            merged_tensor = model.predict([sentence_tensor[:, :, 2:],
+                                           sentence_tensor[:, :, 0],
+                                           sentence_tensor[:, :, 1]])
+            assert_array_equal(sentence_tensor, merged_tensor)
+
+    def test_merge_adds_words_to_sentence_correctly(self):
+        # The thing to note here is that when we're adding words, we're adding rows to the mask as
+        # well.  This test makes sure that this works correctly.
+        vocab_size = 10
+        sentence_length = 3
+        word_length = 3
+        embedding_dim = 10
+        sentence_input = Input(shape=(sentence_length, word_length), dtype='int32')
+        extra_word_input = Input(shape=(word_length,), dtype='int32')
+        embedding = TimeDistributedEmbedding(input_dim=vocab_size, output_dim=embedding_dim, mask_zero=True)
+        embedded_sentence = embedding(sentence_input)  # (batch_size, sentence_length, word_length, embedding_dim)
+        embedded_extra_word = embedding(extra_word_input)  # (batch_size, word_length, embedding_dim)
+        merge_layer = VectorMatrixMerge(concat_axis=1)
+        merged_sentence = merge_layer([embedded_extra_word, embedded_sentence])
+        result_mask = OutputMask()(merged_sentence)
+        model = Model(inputs=[sentence_input, extra_word_input], outputs=[merged_sentence, result_mask])
+        sentence_tensor = numpy.asarray([[[1, 3, 0], [2, 8, 7], [0, 0, 0]]])
+        extra_word_tensor = numpy.asarray([[9, 0, 0]])
+        merged_tensor, result_mask_tensor = model.predict([sentence_tensor, extra_word_tensor])
+        expected_mask = numpy.asarray([[[1, 0, 0], [1, 1, 0], [1, 1, 1], [0, 0, 0]]])
+        assert merged_tensor.shape == (1, sentence_length + 1, word_length, embedding_dim)
+        assert_array_equal(result_mask_tensor, expected_mask)
+
+    def test_merge_adds_dims_to_word_embedding_correctly(self):
+        # The thing to note here is that when we're adding dimensions to an embedding, we're not
+        # changing the mask.  That is, the concat axis is greater than the dimensionality of the
+        # mask.  This test makes sure that this works correctly.
+        vocab_size = 10
+        sentence_length = 6
+        embedding_dim = 10
+        for concat_axis in [2, -1]:
+            sentence_input = Input(shape=(sentence_length,), dtype='int32')
+            extra_embedding_input = Input(shape=(sentence_length,), dtype='float32')
+            embedding = TimeDistributedEmbedding(input_dim=vocab_size, output_dim=embedding_dim, mask_zero=True)
+            embedded_sentence = embedding(sentence_input)  # (batch_size, sentence_length, embedding_dim)
+            merge_layer = VectorMatrixMerge(concat_axis=concat_axis)
+            merged_sentence = merge_layer([extra_embedding_input, embedded_sentence])
+            result_mask = OutputMask()(merged_sentence)
+            model = Model(inputs=[sentence_input, extra_embedding_input], outputs=[merged_sentence, result_mask])
+            sentence_tensor = numpy.asarray([[1, 3, 6, 2, 0, 0]])
+            extra_word_tensor = numpy.asarray([[1, 2, 3, 4, 5, 6]])
+            merged_tensor, result_mask_tensor = model.predict([sentence_tensor, extra_word_tensor])
+            expected_mask = numpy.asarray([[1, 1, 1, 1, 0, 0]])
+            assert merged_tensor.shape == (1, sentence_length, embedding_dim + 1)
+            assert_array_equal(merged_tensor[0, :, 0], [1, 2, 3, 4, 5, 6])
+            assert_array_equal(result_mask_tensor, expected_mask)
