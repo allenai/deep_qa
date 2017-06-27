138a25fcd83c10be0fea924a9f462e26bde09ee9
Roy Schwartz <roys@roy-dev.dev.ai2>
2017-06-27 18:24:06 +0000

    Added support for generating predictions

--- scripts/run_model.py
+++ scripts/run_model.py
@@ -1,17 +1,18 @@
 import logging
 import os
 import sys
+import numpy
 
 # pylint: disable=wrong-import-position
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from deep_qa import run_model, evaluate_model
+from deep_qa import run_model, evaluate_model, score_dataset
 from deep_qa.common.checks import ensure_pythonhashseed_set
 
 logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 
 
 def main():
-    usage = 'USAGE: run_model.py [param_file] [train|test]'
+    usage = 'USAGE: run_model.py [param_file] [train|test|pred (ofile)]'
     if len(sys.argv) == 2:
         run_model(sys.argv[1])
     elif len(sys.argv) == 3:
@@ -23,6 +24,12 @@ def main():
         else:
             print(usage)
             sys.exit(-1)
+    elif len(sys.argv) == 4 and sys.argv[2] == 'pred':
+        print("Predicting!")
+        predictions, labels = score_dataset(sys.argv[1])
+
+        with open(sys.argv[3], "wb") as ofh:
+            numpy.savez(ofh, numpy.array([predictions,labels]))
     else:
         print(usage)
         sys.exit(-1)
